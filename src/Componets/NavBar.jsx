aae9a86dc0cf5f98920abb587bb8c0f091cd996d
anandhusb444 <anandhusb444@gmail.com>
2025-03-15 12:02:06 +0530

    add nav base style

--- src/Componets/NavBar.jsx
+++ src/Componets/NavBar.jsx
@@ -3,40 +3,45 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-      <nav style={{backgroundColor: '#0C270C'}} className='w-600 h-18' >
-        <div className='flex p-5 '>
-        <div className='text-white'>
-            Movie Kona
-        </div>
-        <div>
+      <nav style={{backgroundColor: '#0C270C'}} className='h-18' >
+        <div className='flex p-4'>
+        <div className='text-white relative left-7'>Movie</div>
+        <div className='relative top-4 left-3 text-[#7AB41D]'>Kona</div>
+        <div className='relative left-20 top-0.5'>
             <input type='text'
             placeholder='Search'
             className='rounded-lg border
+              h-8
+              w-63
              border-gray-600
               bg-white
               text-white 
               focus:outline-none 
               focus:ring-2 focus:ring-blue-500"'
+              
             >
             </input>
         </div>
-        <div className='text-white'>
-            Movies
+        <div className='text-white relative left-90 p-2 text-sm'>
+            About
         </div>
-        <div className='text-white'>
-            TV Show
+        <div className='text-white relative left-90 p-2 text-sm'>
+            Teams
         </div>
-        <div className='text-white'>
-            Fans Show
+        <div className='text-white relative left-90 p-2 text-sm'>
+          Fanart
         </div>
-        <div>
-            <button className='bg-white rounded-lg border text-[#7AB41D]' >
+        <div className='text-white relative left-90 p-2 text-sm'>
+          News
+        </div>
+        <div className='relative left-100 p-0.5'>
+            <button className='bg-white rounded-4xl border text-[#7AB41D] w-28 h-9 font-light' >
                 USER LOGIN
             </button>
         </div>
-        <div>
-            <button className='bg-[#7AB41D] rounded-lg border text-white'>
-                Watch List
+        <div className='relative left-105 p-0.5'>
+            <button className='bg-[#7AB41D] rounded-4xl  text-white w-23 h-9 font-light'>
+                LOGO
             </button>
         </div>
             

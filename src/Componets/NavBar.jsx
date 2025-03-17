ad56c123a853fd0e10d374f6e1a5f487cc0e7bbb
anandhusb444 <anandhusb444@gmail.com>
2025-03-17 22:49:42 +0530

    Added the hovering effect

--- src/Componets/NavBar.jsx
+++ src/Componets/NavBar.jsx
@@ -5,8 +5,8 @@ const NavBar = () => {
     <div>
       <nav style={{backgroundColor: '#0C270C'}} className='h-18' >
         <div className='flex p-4'>
-        <div className='text-white relative left-7'>Movie</div>
-        <div className='relative top-4 left-3 text-[#7AB41D]'>Kona</div>
+        <div className='text-white relative left-7 font-mono  '>Movie</div>
+        <div className='relative top-4 left-3 text-[#7AB41D] font-mono'>Kona</div>
         <div className='relative left-20 top-0.5'>
             <input type='text'
             placeholder='Search'
@@ -22,25 +22,30 @@ const NavBar = () => {
             >
             </input>
         </div>
-        <div className='text-white relative left-90 p-2 text-sm'>
+        <div className='text-white relative left-90 p-2 text-sm font-mono'>
             About
         </div>
-        <div className='text-white relative left-90 p-2 text-sm'>
+        <div className='text-white relative left-90 p-2 text-sm font-mono'>
             Teams
         </div>
-        <div className='text-white relative left-90 p-2 text-sm'>
+        <div className='text-white relative left-90 p-2 text-sm font-mono'>
           Fanart
         </div>
-        <div className='text-white relative left-90 p-2 text-sm'>
-          News
+        <div className='text-white relative left-90 p-2 hover:-translate-y-1 transition-transform duration-300'>
+          <span className='font-mono text-sm '>
+              News
+          </span>
+
         </div>
         <div className='relative left-100 p-0.5'>
             <button className='bg-white rounded-4xl border text-[#7AB41D] w-28 h-9 font-light' >
+              <span className="text-[#7AB41D] hover:text-[#0C270C] transition-colors duration-200 font-mono">
                 USER LOGIN
+              </span>
             </button>
         </div>
         <div className='relative left-105 p-0.5'>
-            <button className='bg-[#7AB41D] rounded-4xl  text-white w-23 h-9 font-light'>
+            <button className='bg-[#7AB41D] rounded-4xl  text-white w-23 h-9 font-light font-mono'>
                 LOGO
             </button>
         </div>

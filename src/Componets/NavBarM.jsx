4da92dc5da667d3225d8668db690d809c3be9acd
anandhusb444 <anandhusb444@gmail.com>
2025-06-03 22:16:35 +0530

    Added flex box nav bar

---
+++ src/Componets/NavBarM.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function NavBar1() {
+  return (
+    <div>
+      <nav>
+        <div>
+            <img/>
+            <button></button>
+        </div>
+
+        <div>
+            <a>About</a>
+            <a>Teams</a>
+            <a>Fanart</a>
+            <a>News</a>
+        </div>
+
+        <div>
+            <button>USER LOGIN</button>
+            <button>Register</button>
+        </div>
+
+      </nav>
+    </div>
+  )
+}

e22f60dbe2ac58d6ea2635184a6748b62a8d5b66
anandhusb444 <anandhusb444@gmail.com>
2025-06-27 16:29:59 +0530

    Completed 1stage of navBar added the button and the links and search bar

--- src/Componets/NavBarM.jsx
+++ src/Componets/NavBarM.jsx
@@ -5,20 +5,26 @@ export default function NavBar1() {
     <div>
       <nav className='flex justify-around items-center w-screen h-20 bg-[#0C270C]' >
         <div className='flex justify-around align-middle '>
-            <img src='' alt='logo'/>
-            <button>ser</button>
+            <p className='text-white font-mono'>Movie</p>
+            <p className='text-[#7AB41D] font-mono relative top-3 right-4'>Kona</p>
+
+          <input
+              type='text'
+              placeholder='Search'
+              className='rounded-lg  border-white text-black px-2 bg-white w-50 h-7 ml-24 '>
+          </input>
         </div>
 
-        <div className='flex justify-around items-center'>
-            <a>About</a>
-            <a>Teams</a>
-            <a>Fanart</a>
-            <a>News</a>
+        <div className='flex justify-around items-center text-white gap-3.5 font-mono relative left-18'>
+            <a className='hover:-translate-y-1 hover:text-[#ffd700] transition-transform duration-300'>About</a>
+            <a className='hover:-translate-y-1 hover:text-[#ffd700] transition-transform duration-300'>Teams</a>
+            <a className='hover:-translate-y-1 hover:text-[#ffd700] transition-transform duration-300'>Fanart</a>
+            <a className='hover:-translate-y-1 hover:text-[#ffd700] transition-transform duration-300'>News</a>
         </div>
 
-        <div>
-            <button>USER LOGIN</button>
-            <button>Register</button>
+        <div className='flex justify-around items-center text-white gap-3.5 font-mono'>
+            <button className='text-[#7AB41D] bg-white w-23 h-8 rounded-2xl'>Profile</button>
+            <button className='bg-[#7AB41D] w-23 h-8 rounded-2xl'>LOGIN</button>
         </div>
 
       </nav>
